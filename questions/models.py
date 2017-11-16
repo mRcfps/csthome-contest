2a19c4b5fb8a188460f5930f4011472c4c50bc1a
mRcfps <1402491442@qq.com>
2017-11-16 21:50:42 +0800

    [ADD] single and multiple question model

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Single(models.Model):
+
+    question = models.CharField(max_length=200)
+    answer = models.CharField(max_length=1)
+
+    class Meta:
+        verbose_name = "单选题"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.question.split("\n")[0]
+
+
+class Multiple(models.Model):
+
+    question = models.CharField(max_length=200)
+    answers = models.CharField(max_length=4)
+
+    class Meta:
+        verbose_name = "多选题"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.question.split("\n")[0]
