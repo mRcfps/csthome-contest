85f85ce9c918b3ffa97612330fa6c57bded54be3
mRcfps <1402491442@qq.com>
2017-11-17 17:13:01 +0800

    [FIX] url regex bug (missing $)

--- questions/urls.py
+++ questions/urls.py
@@ -4,12 +4,12 @@ from . import views
 
 urlpatterns = [
     url(
-        r'^single/(?P<pk>\d+)/',
+        r'^single/(?P<pk>\d+)/$',
         views.SingleQuestionView.as_view(),
         name='single_detail'
     ),
     url(
-        r'^multiple/(?P<pk>\d+)/',
+        r'^multiple/(?P<pk>\d+)/$',
         views.MutipleQuestionView.as_view(),
         name='multiple_detail'
     ),
