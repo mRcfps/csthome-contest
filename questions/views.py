e43d5d80ecf52d6f0b33077aa1dd6941bd4433ce
mRcfps <1402491442@qq.com>
2017-11-17 11:33:12 +0800

    [ADD] views of questions app

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models, serializers
+
+
+class SingleQuestionView(generics.RetrieveAPIView):
+
+    queryset = models.Single.objects.all()
+    serializer_class = serializers.SingleSerializer
+
+
+class MutipleQuestionView(generics.RetrieveAPIView):
+
+    queryset = models.Multiple.objects.all()
+    serializer_class = serializers.MultipleSerializer
