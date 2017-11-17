e53eef550739a3870d6dd4eee1b6351fb6cc297c
mRcfps <1402491442@qq.com>
2017-11-17 11:31:09 +0800

    [ADD] urlconf of questions app

---
+++ questions/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(
+        r'^single/(?P<pk>\d+)/',
+        views.SingleQuestionView.as_view(),
+        name='single_detail'
+    ),
+    url(
+        r'^multiple/(?P<pk>\d+)/',
+        views.MutipleQuestionView.as_view(),
+        name='multiple_detail'
+    ),
+]
