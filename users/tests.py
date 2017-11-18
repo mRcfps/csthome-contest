e6fe051fef7e85da22da9173c76d3bf25295e048
mRcfps <1402491442@qq.com>
2017-11-18 18:17:36 +0800

    [ADD] unit tests for users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,45 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from .models import Contestant
+
+
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', password='test'
+        )
+        self.client = APIClient()
+
+    def test_login(self):
+        url = reverse('users:login')
+        data = {'username': 'test', 'password': 'test'}
+        response = self.client.post(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(response.data['token'], None)
+
+
+class ContestantTests(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='test', password='test'
+        )
+        self.contestant = Contestant.objects.create(
+            user=self.user,
+            name='marc',
+            score=50
+        )
+        self.client = APIClient()
+
+    def test_get_score_board(self):
+        url = reverse('users:scores')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data[0]['score'], 50)
