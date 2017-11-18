68f720638ca2255cd79a1b4178859e9f79356a75
mRcfps <1402491442@qq.com>
2017-11-18 18:30:31 +0800

    [ADD] views docstring displayed in docs

--- questions/views.py
+++ questions/views.py
@@ -4,12 +4,14 @@ from . import models, serializers
 
 
 class SingleQuestionView(generics.RetrieveAPIView):
+    """根据序号获取单选题（1~90）"""
 
     queryset = models.Single.objects.all()
     serializer_class = serializers.SingleSerializer
 
 
 class MutipleQuestionView(generics.RetrieveAPIView):
+    """根据序号获取多选题（1~47）"""
 
     queryset = models.Multiple.objects.all()
     serializer_class = serializers.MultipleSerializer
