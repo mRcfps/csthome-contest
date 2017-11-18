af18fd3a11bba181af285f942cb1021a47a3225b
mRcfps <1402491442@qq.com>
2017-11-18 18:08:19 +0800

    [ADD] login and scores view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Contestant
+from .serializers import ContestantSerializer
+
+
+class UserLoginView(APIView):
+    """用户登录"""
+
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ScoreBoardView(generics.ListAPIView):
+    """获取所有选手的比分"""
+
+    serializer_class = ContestantSerializer
+    queryset = Contestant.objects.all()
