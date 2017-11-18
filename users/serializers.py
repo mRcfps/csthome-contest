384d720ddcbbd0fcd40854bee1f18a8739e4a890
mRcfps <1402491442@qq.com>
2017-11-18 17:59:41 +0800

    [ADD] serializers of user and contestant

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Contestant
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
+
+
+class ContestantSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Contestant
+        fields = ('name', 'score')
