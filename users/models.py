84d0fa0a9c6fc0961f22b4cf175664977ceb590d
mRcfps <1402491442@qq.com>
2017-11-18 17:57:50 +0800

    [ADD] contestant model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contestant(models.Model):
+
+    user = models.OneToOneField(User)
+    name = models.CharField(max_length=10)
+    score = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ('-score')
+        verbose_name = "参赛者"
+        verbose_name_plural = verbose_name
